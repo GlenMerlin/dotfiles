ab7f5634224ed456e4f4c5677264609f6f96a39b
GlenMerlin <Glenmerlin@protonmail.com>
2021-10-20 00:06:56 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Place config and prompt in proper locations
+sudo ln -s $PWD/manjaro-zsh-config /usr/share/zsh/manjaro-zsh-config
+sudo ln -s $PWD/manjaro-zsh-prompt /usr/share/zsh/manjaro-zsh-prompt
+
+# Place .gitconfig and .zshrc in the home directory
+ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/.zshrc ~/.zshrc
+
+# Place pacman.conf in /etc
+sudo ln -s $PWD/pacman.conf /etc/pacman.conf
+
+# Set default mirrorlist for pacman
+sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
+
+# Install yay
+pacman -S --needed git base-devel yay
+
+# Check for updates
+echo "y" | yay
+
+# Set up folder for .AppImages
+mkdir ~/Applications
+
+# Move AppImage .desktop files to their proper location
+sudo cp $PWD/desktop-files/bitwarden.desktop /usr/share/applications
+sudo cp $PWD/desktop-files/standard-notes.desktop /usr/share/applications
+
+# Install Snap
+echo "y" | yay -S snapd
+
+# Install Snaps
+sudo snap install authy
+sudo snap install spotify
+sudo snap install termius-app
+sudo snap install todoist
+sudo snap refresh
+
+# Install Flatpak
+echo "y" | yay -S flatpak
+
+# Install Flatpak apps
+sudo flatpak install com.mojang.Minecraft
+sudo flatpak install com.obsproject.Studio
+sudo flatpak install me.okzec.syncthingtk
+
+# Clean out unused packages
+yay --clean
+
+# Echo Manual Setup instructions
+echo "Download Bitwarden.AppImage and Standard-Notes.AppImage and place them in /home/glenmerlin/Applications"
