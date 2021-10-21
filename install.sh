d481db82dfb7ef8a1f7409b51e9fa440a2125a1b
GlenMerlin <Glenmerlin@protonmail.com>
2021-10-20 19:23:46 -0700

    fix failed symlinks in install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Place config and prompt in proper locations
+sudo rm /usr/share/zsh/manjaro-zsh-config
+sudo rm /usr/share/zsh/manjaro-zsh-prompt
 sudo ln -s $PWD/manjaro-zsh-config /usr/share/zsh/manjaro-zsh-config
 sudo ln -s $PWD/manjaro-zsh-prompt /usr/share/zsh/manjaro-zsh-prompt
 
 # Place .gitconfig and .zshrc in the home directory
 ln -s $PWD/.gitconfig ~/.gitconfig
+rm ~/.zshrc
 ln -s $PWD/.zshrc ~/.zshrc
 
 # Place pacman.conf in /etc
+sudo rm /etc/pacman.conf
 sudo ln -s $PWD/pacman.conf /etc/pacman.conf
 
 # Set default mirrorlist for pacman
-sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
+sudo pacman-mirrors --fasttrack && echo "y" | sudo pacman -Syyu
 
 # Install yay
-pacman -S --needed git base-devel yay
+echo "y" | pacman -S --needed git base-devel yay
 
 # Check for updates
 echo "y" | yay
